9eed5c8e0474a5436e9e823cb0d08bf6ba360daf
tim10221 <ejrhgk16@naver.com>
2026-05-25 17:54:12 +0900

    Revert "plan-63-fix-circular-sensor-steering-cast"
    This reverts commit 25b812235078357347f197bd65d8fdba31fca791.

--- src/services/Pursuer.ts
+++ src/services/Pursuer.ts
@@ -226,7 +226,7 @@ export class Pursuer {
         rFromContact = Math.hypot(this.x - last.x, this.y - last.y);
       }
       const effectiveSigma = this.sigma + this.trackingParams.kRadial * rFromContact;
-      if (this.sigma > this.trackingParams.castThreshold) {
+      if (this.sigma > this.trackingParams.theta0) {
         this.state = 'cast';
         this.castOriginX = this.x;
         this.castOriginY = this.y;
@@ -275,7 +275,7 @@ export class Pursuer {
         moveSpeed = this.trackingParams.maxSpeed;
       }
     } else if (this.state === 'track') {
-      this.targetHeading = sample.signalDirection;
+      this.targetHeading = this.rotationAngle + (Math.PI / 6) * sample.netBias;
 
       if (this.isReversingTrail()) {
         this.targetHeading = this.normalizeAngle(this.targetHeading + Math.PI);
